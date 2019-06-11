34d2fbb86d77667007ec7bbc0a7b97aedd73cc35
AnuradhaSK <anuradha199528@gmail.com>
2019-06-11 07:33:05 +0530

    Added functionality to write title and singer into a json file

--- lyrics/spiders/lyrics_spider.py
+++ lyrics/spiders/lyrics_spider.py
@@ -6,11 +6,16 @@ class LyricsSpider(SitemapSpider):
     sitemap_urls = [
         'http://lyricslk.com/sitemap.xml'
     ]
-    sitemap_rules = [('/lyrics/artist/', 'parse')]
-    sitemap_follow = ['/amaradewa-w-d']
+    # crawls links which does not contain artist. But this outputs some links like submit page etc
+    sitemap_rules = [('^(?!.*artist).*', 'parse')]
+    # sitemap_follow = ['^((?!artist).)*$']
     def parse(self, response):
-        page = response.url.split("/")[-1]
-        filename = 'lyrics-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        yield {
+            'title': response.xpath('//*[@id="lyricsTitle"]/h2/text()')[0].get().split('- ')[0],
+            'singer': response.xpath('//*[@id="lyricsTitle"]/h2/text()')[0].get().split('- ')[1],
+        }
+        # page = response.url.split("/")[-1]
+        # filename = 'lyrics-%s.html' % page
+        # with open(filename, 'wb') as f:
+        #     f.write(response.body)
 
